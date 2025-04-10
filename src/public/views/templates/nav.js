e0673952236b87e4f2facdc20cc2f0f94c5ed6bf
Luana Elverdi <luelverdi02@gmail.com>
2025-04-10 18:24:05 -0300

    template nuevo

---
+++ src/public/views/templates/nav.js
@@ -0,0 +1,18 @@
+export const TEMPLATE_NAVIGATION = `
+    <div class="menu">
+        <ul class="nav nav-tabs">
+            <li class="nav-item m-2">
+                <a class="nav-link active" href="/" data-link>INICIO</a>
+            </li>
+            <li class="nav-item m-2">
+                <a class="nav-link active" href="/productos" data-link>PRODUCTOS</a>
+            </li>
+            <li class="nav-item m-2">
+                <a class="nav-link active" href="/servicios" data-link>SERVICIOS</a>
+            </li>
+            <li class="nav-item m-2">
+                <a class="nav-link active" href="/contacto" data-link>CONTACTO</a>
+            </li>
+        </ul>
+    </div>
+`;
